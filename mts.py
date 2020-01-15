226d5cc2a08af5be0dce7011ad4287880483b78f
kostekd <45406173+kostekd@users.noreply.github.com>
2020-01-15 22:31:50 +0100

    Update mts.py

--- mts.py
+++ mts.py
@@ -1,68 +1,229 @@
-def onp_module():
-    def findOperators(operators, sen):
-        operators
-        for i in range(len(operators)):
-            if operators[i] == sen:
-                return operators[i]
+# chwdp
+def exactType(a):
+    const = "abcde"
+    var = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    typedict = {
+        "NOT": "not",
+        "~": "not",
+        "¬": "not",
+        "AND": "and",
+        "&": "and",
+        "∧": "and",
+        "OR": "or",
+        "|": "or",
+        "∨": "or",
+        "IMPLIES": "implies",
+        "→": "implies",
+        "IFF": "iff",
+        "↔": "iff",
+        "XOR": "xor",
+        "⊕": "xor",
+        "FORALL": "forall",
+        "∀": "forall",
+        "EXISTS": "exists",
+        "∃": "exists"
+    }
+    if a in const:
+        return "CONST"
+    elif a in var:
+        return "VAR"
+    else:
+        return typedict.get(a)
 
-    def getArguments(function, sen, index):
-        numb = int(function[function.find('/') + 1:])
-        new_sub_sentence = function[0] + "("
-        for i in range(index - numb, index):
-            if i != index - 1:
-                new_sub_sentence += sen[i] + ", "
-            else:
-                new_sub_sentence += sen[i]
-        new_sub_sentence += ")"
 
-        sen = sen[:index - numb] + [new_sub_sentence] + sentence[index + 1:]
-        return sen
+def typ(a):
+    if ("/" in a):
+        return 0  # predykat lub funkcja
+    if (len(a) == 1 and (ord(a) >= ord("a") and ord(a) <= ord("e")) or (
+            len(a) == 1 and ord(a) >= ord("A") and ord(a) <= ord("Z"))):
+        return 1  # stała lub zmienna
+    if (a == "FORALL" or a == "EXISTS" or a == "→" or a == "∃"):
+        return 2  # kwantyfikator
+    if (a == "NOT" or a == "¬" or a == "~"):
+        return 3  # negacja
+    return 4  # reszta
 
-    def getOperators(operator, sen, index):
-        numb = 2
-        if operator == "FORALL" or operator == "∀" or operator == "EXISTS" or operator == "∃":
-            new_sub_sentence = '(' + operator + " " + sen[index - 2] + " " + sen[index - 1] + ')'
-        elif operator == "NOT" or operator == "~" or operator == "¬":
-            numb = 1
-            new_sub_sentence = '(' + operator + " " + sen[index - 1] + ')'
-        else:
-            new_sub_sentence = '(' + sen[index - 2] + " " + operator + " " + sen[index - 1] + ')'
 
-        sen = sen[:index - numb] + [new_sub_sentence] + sentence[index + 1:]
-        return sen
+def operator(index):
+    global x
+    x = x[:index - 2] + [[x[index - 2]] + [x[index - 1]] + [x[index]]] + x[index + 1:]
+    # x=x[:index-2]+["("+x[index-2]+" "+x[index]+" "+x[index-1]+")"]+x[index+1:]
+    return 2
 
-    OPERATORS = ["NOT", "¬", "~", "AND", "&", "∧", "OR", "|", "∨", "IMPLIES", "→", "IFF", "↔", "XOR", "⊕", "FORALL",
-                 "∀", "EXISTS", "∃"]
-    sentence = input().split()
-    i = 0
-    while i < len(sentence):
-        operator = findOperators(OPERATORS, sentence[i])
-        if operator != None:
-            sentence = getOperators(operator, sentence, i)
-            if operator == "NOT" or operator == "~" or operator == "¬":
-                i -= 1
-            else:
-                i -= 2
-        elif sentence[i].find('/') != -1:
-            numb = int(sentence[i][sentence[i].find('/') + 1:])
-            sentence = getArguments(sentence[i], sentence, i)
-            i -= numb
-        i += 1
-    return " ".join(sentence)
-'''
+
+def negacja(index):
+    global x
+    x = x[:index-1]+ [[x[index-1]] + [x[index]]] + x[index+1:]
+    return 1
+
+def kwantyfikator(index):
+    global x
+    if len(x) - 1 == index:
+        x = [x[:index - 1] + [x[index - 1]]] + x[index:]
+    else:
+        x = x[:index - 2] + [[[x[index - 2]] + [x[index - 1]]] + [x[index]]] + x[index + 1:]
+    return 2
+
+def done():
+    global x
+
+    if(typ(x[len(x)-1])==3):
+        if(len(x)==2):
+            return 1
+        return 0
+    elif(typ(x[len(x)-1])==0):
+        funkcja_predykat(len(x)-1)
+        x=x[0]
+        return 1
+    elif(typ(x[len(x)-1])==2):
+        if (len(x) < 3):
+            return 1
+        return 0
+    else:
+        if (len(x) == 3):
+            return 1
+        return 0
+
+def funkcja_predykat(index):
+    global x
+    newindeks = int(x[index][2:])
+    znakfunkcji = x[index][0]
+    newstr="".join(x[index-newindeks:index])
+    stala=[]
+    for i in range(newindeks):
+        stala.append(newstr[i])
+    x=x[:index-newindeks] + [stala + [znakfunkcji]] + x[index+1:]
+    #x = x[:index-newindeks]+[znakfunkcji+"("+ newstr +")"]+x[index+1:]
+    return newindeks
+
 def detect(sentence):
-    #NOT IMPLEMENTED
-def apply(expression):
-    #NOT IMPLEMENTED
-def alfa(expression):
-    #NOT IMPLEMENTED
-def beta(expression):
-    #NOT IMPLEMENTED
-def gamma(expression):
-    #NOT IMPLEMENTED
-def delta(expression):
-    #NOT IMPLEMENTED
-'''
+    operation = ''
+    for element in sentence:
+        if element[len(element) - 1] == 'NOT' or element[len(element) - 1] == '~' or element[len(element) - 1] == '¬':
+            operation = exactType(element[0][len(element[0]) - 1])
+            if operation == "or" or operation == 'implies' or operation == 'xor':
+                return 'alfa'
+            # detect beta
+            elif operation == "and" or operation == "iff":
+                return 'beta'
+            # detect delta
+            elif operation == 'forall':
+                return 'delta'
+            # detect gamma
+            elif operation == 'exists':
+                return 'gamma'
+            else:
+                return 'Nothing'
+        else:
+            operation = exactType(element[len(element) - 1])
+            # detect alfa
+            if operation == "and" or operation == 'iff':
+                return 'alfa'
+            # detect beta
+            elif operation == "or" or operation == "implies" or operation == 'xor':
+                return 'beta'
+            # detect gamma
+            elif operation == 'forall':
+                return 'gamma'
+            # detect delta
+            elif operation == 'exists':
+                return 'delta'
+            else:
+                return 'Nothing'
 
+def extractVariable(sentence):
+    return sentence[0][0]
+def getConst(con):
+    #NOT IMPLEMENTED, FOR NOW RETURNING ELEMENT 0
+    return con[0]
+#RULES
+def alfa(expression):
+    operator = exactType(expression[len(expression) - 1])
+    if operator == 'and':
+        return [expression[0]] + [expression[1]]
+    elif operator == 'or':
+        #due to presence of negation
+        return [[expression[0]] + ['NOT']] + [[expression[1]] + ['NOT']]
+    elif operator == 'implies':
+        return [expression[0]] + [[expression[1]] + ['NOT']]
+    elif operator == 'iff' or operator == 'xor':
+        return [[expression[0]] + [expression[1]] + ['IMPLIES']] + [[expression[1]] + [expression[0]] + ['IMPLIES']]
+
+def beta(expression):
+    operator = exactType(expression[len(expression) - 1])
+    if operator == 'and':
+        return [[expression[0]] + ['NOT']] + [[expression[1]] + ['NOT']]
+    elif operator == 'or':
+        return [expression[0]] + [expression[1]]
+    elif operator == 'implies':
+        return [expression[0] + ['NOT']] + [expression[1]]
+    elif operator == 'iff' or expression == 'xor':
+        return[[[expression[0]] + [expression[1]] + ['IMPLIES']] + ['NOT']] + [[[expression[1]] + [expression[0]] + ['IMPLIES']] + ['NOT']]
+
+def gamma(a, const, var):
+    for i in range(0,len(a)):
+        if type(a[i]) == list:
+            gamma(a[i], const, var)
+        else:
+            if a[i] == var:
+                a[i] = const
+
+    return a[0]
+
+#================================================
+def apply(exp, rule, con = None, var = None):
+    if rule == 'alfa':
+        return alfa(exp)
+    elif rule == 'beta':
+        return beta(exp)
+    elif rule == 'gamma':
+        return gamma(exp, con, var)
+    else:
+        return 0
 if __name__ == "__main__":
-    suffix = onp_module()
+    constants = ['a']
+    my_const, my_var = None, None
+    i = 0
+    leafs = []
+    x = input().split()
+
+    while(not done()):
+        if (typ(x[i]) == 0):
+            i -= funkcja_predykat(i)
+        elif (typ(x[i]) == 2):
+            i -= kwantyfikator(i)
+        elif (typ(x[i]) == 3):
+            i -= negacja(i)
+        elif (typ(x[i]) == 4):
+            i -= operator(i)
+        i += 1
+    print(x)
+    # testing leaf
+    leafs.append([x])
+    current = 0
+    for i in range (len(leafs)):
+        z = detect(leafs[i])
+
+        #extracting variable that will be changed with constant (GAMMA)
+        if z == 'gamma':
+            my_var = extractVariable(leafs[i][0])
+            my_const = getConst(constants)
+            
+
+        if exactType(leafs[i][current][len(leafs[i][current]) - 1]) == 'not':
+            result = apply(leafs[i][current][0], z, my_const, my_var)
+            if z == 'beta':
+                leafs[i] = result[0]
+                leafs.append([result[1]])
+            else:
+                leafs[i] = result
+
+        else:
+            result = apply(leafs[i][current], z, my_const, my_var)
+            print(result)
+            if z == 'beta':
+                leafs[i] = result[0]
+                leafs.append([result[1]])
+            else:
+                leafs[i] = result
+print(leafs)
